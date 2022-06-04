358071d1ac85d88f45565a923ada3c10db8385e7
Igor Menezes <igorsilvamenezes@gmail.com>
2022-06-03 21:03:30 -0300

    created the exemple fig08_09

---
+++ chapter_08/fig08_09.c
@@ -0,0 +1,19 @@
+/* Figura 8.8: fig08_09.c
+	Usando strtod */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( void )
+{
+	/* inicializa ponteiro de string */
+	const char *string = "51.2% sao admitidos"; /* inicializa string */
+	
+	double d; /* variável para manter sequencia convertida */
+	char *stringPtr; /* cria ponteiro char */
+	
+	d = strtod( string, &stringPtr );
+	
+	printf( "A string \"%s\" eh convertida em \n", string );
+	printf( "valor double %.2f e a string \"%s\"\n", d, stringPtr );
+	return 0; /* indica conclusão bem-sucedida */
+} /* fim do main */
